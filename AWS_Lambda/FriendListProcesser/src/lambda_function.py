931c7a562ad66bb9586bdf5745cf116f143273b7
namoshika <namoshika@users.noreply.github.com>
2022-11-06 22:29:43 +0900

    Notion への書出し処理を追加

---
+++ AWS_Lambda/FriendListProcesser/src/lambda_function.py
@@ -0,0 +1,161 @@
+import boto3
+import json
+import logging
+import os
+from common import entity, dynamodb
+from datetime import datetime
+from notion_client import Client
+
+logger = logging.getLogger(__name__)
+
+
+class NotionResource:
+    def __init__(self, database_put_id: str, auth_token: str):
+        self._client = Client(auth=auth_token)
+        self._database_put_id = database_put_id
+
+    def update(self, op: entity.OperationInfo, location: str) -> dict:
+        target_info = op.info_new if op.action != entity.ActionType.REMOVE else op.info_old
+        record = self._inline_find(target_info.user_id)
+        if record is None:
+            return self._inline_append(target_info, op.action == entity.ActionType.REMOVE, location)
+        else:
+            return self._inline_update(target_info, op.action == entity.ActionType.REMOVE, record)
+
+    def _inline_find(self, user_id) -> dict:
+        # 更新対象のレコードを取得
+        query_res = self._client.databases.query(
+            database_id=self._database_put_id,
+            filter={"property": "user_id", "rich_text": {"equals": user_id}},
+            page_size=1
+        )
+        if len(query_res["results"]) > 0:
+            return query_res["results"][0]
+        else:
+            return None
+
+    def _inline_append(self, value: entity.FriendInfo, mark_as_removed: bool, location: str) -> dict:
+        res = self._client.pages.create(**{
+            "parent": {
+                "type": "database_id",
+                "database_id": self._database_put_id
+            },
+            "properties": {
+                "user_name": {
+                    "type": "title",
+                    "title": [{"type": "text", "text": {"content": value.user_name}}]
+                },
+                "user_name_displayed": {
+                    "type": "rich_text",
+                    "rich_text": [{"type": "text", "text": {"content": value.user_display_name}}]
+                },
+                "user_id": {
+                    "type": "rich_text",
+                    "rich_text": [{"type": "text", "text": {"content": value.user_id}}]
+                },
+                "location": {
+                    "type": "rich_text",
+                    "rich_text": [{"type": "text", "text": {"content": location}}]
+                },
+                "regist_date": {
+                    "type": "date",
+                    "date": {"start": datetime.fromtimestamp(value.regist_date).strftime("%Y-%m-%d")} if value.regist_date is not None else None
+                },
+                "update_date": {
+                    "type": "date",
+                    "date": {"start": datetime.fromtimestamp(value.update_date).strftime("%Y-%m-%d")} if value.update_date is not None else None
+                },
+                "removed_date": {
+                    "type": "date",
+                    "date": {"start": datetime.fromtimestamp(value.update_date).strftime("%Y-%m-%d")} if mark_as_removed else None
+                }
+            }
+        })
+        return res
+
+    def _inline_update(self, value: entity.FriendInfo, mark_as_removed: bool, page_obj: dict) -> dict:
+        # レコードの情報を取得
+        page_id = page_obj["id"]
+        page_props = page_obj["properties"]
+
+        # レコードの情報を更新
+        # page_props["user_name"] = {
+        #     "type": "title",
+        #     "title": [{"type": "text", "text": {"content": value.user_name}}]
+        # }
+        page_props["user_name_displayed"] = {
+            "type": "rich_text",
+            "rich_text": [{"type": "text", "text": {"content": value.user_display_name}}]
+        }
+        # page_props["user_id"] = {
+        #     "type": "rich_text",
+        #     "rich_text": [{"type": "text", "text": {"content": value.user_id}}]
+        # }
+        # page_props["location"] = {
+        #     "type": "rich_text",
+        #     "rich_text": [{"type": "text", "text": {"content": location}}]
+        # }
+        # page_props["regist_date"] = {
+        #     "type": "date",
+        #     "date": {"start": datetime.fromtimestamp(value.regist_date).strftime("%Y-%m-%d")}
+        # }
+        page_props["update_date"] = {
+            "type": "date",
+            "date": {"start": datetime.fromtimestamp(value.update_date).strftime("%Y-%m-%d")} if value.update_date is not None else None
+        }
+        page_props["removed_date"] = {
+            "type": "date",
+            "date": {"start": datetime.fromtimestamp(value.update_date).strftime("%Y-%m-%d")} if mark_as_removed else None
+        }
+
+        # レコードの情報を反映
+        res = self._client.pages.update(page_id, properties=page_props)
+        return res
+
+    def _inline_delete(self, page_id: str) -> dict:
+        res = self._client.pages.update(page_id, archived=True)
+        return res
+
+
+class App:
+    def __init__(self, dynamo: dynamodb.Service, notion: NotionResource):
+        self._dynamo = dynamo
+        self._notion = notion
+
+    def invoke(self, op: entity.OperationInfo):
+        if op.action == entity.ActionType.ADD:
+            datetime_event = datetime.fromtimestamp(op.info_new.update_date)
+            latest_encount = self._dynamo.find_latest_activity(op.info_new, datetime_event)
+
+            if latest_encount is not None:
+                # instance_id = latest_encount["instance_id"]
+                # timestamp = latest_encount["timestamp"]
+                # user_name = latest_encount["user_name"]
+                # world_id = latest_encount["world_id"]
+                world_name = latest_encount["world_name"]
+            else:
+                world_name = "(不明)"
+
+            self._notion.update(op, world_name)
+        elif op.action == entity.ActionType.UPDATE or op.action == entity.ActionType.REMOVE:
+            self._notion.update(op, None)
+
+
+def lambda_handler(event, context):
+    APP_GET_TABLE_NAME = os.getenv("APP_GET_TABLE_NAME")
+    APP_PUT_TABLE_NAME = os.getenv("APP_PUT_TABLE_NAME")
+
+    for job in event["Records"]:
+        # キューイングされた情報を取得
+        dec = entity.CustomJsonDecoder()
+        job_info = json.loads(job["body"], object_hook=dec.object_hock)
+        account_id, op_info = job_info["account_id"], job_info["event"]
+
+        ses = boto3.Session()
+        dynamo = dynamodb.Service(account_id, APP_GET_TABLE_NAME, APP_PUT_TABLE_NAME, ses, logger)
+
+        acc = dynamo.get_account()
+        notion = NotionResource(acc.notion_databaseid_friendlist, acc.notion_auth_token)
+
+        app = App(dynamo, notion)
+        app.invoke(op_info)
---
+++ AWS_Lambda/FriendListProcesser/tests/test_lambda_function.py
@@ -0,0 +1,170 @@
+import logging
+import json
+import sys
+import os
+from datetime import datetime
+
+sys.path.append("src")
+logger = logging.getLogger(__name__)
+
+
+class TestNotionResource:
+    def test_update(self):
+        from src import lambda_function
+        from src.common import entity
+
+        NOTION_AUTH_TOKEN = os.getenv("NOTION_AUTH_TOKEN")
+        NOTION_PUT_DATABASEID_FRIENDLIST = os.getenv("NOTION_PUT_DATABASEID_FRIENDLIST")
+        client = lambda_function.NotionResource(NOTION_PUT_DATABASEID_FRIENDLIST, NOTION_AUTH_TOKEN)
+        now_time = int(datetime.now().timestamp())
+
+        val = entity.OperationInfo(
+            lambda_function.entity.ActionType.ADD,
+            entity.FriendInfo("user_id_01", "user_name_01", "user_display_name_01", 0, 0),
+            None
+        )
+        res = client.update(val, "location_01")
+
+        val = entity.OperationInfo(
+            lambda_function.entity.ActionType.UPDATE,
+            entity.FriendInfo("user_id_01", "user_name_02", "user_display_name_02", now_time, now_time),
+            entity.FriendInfo("user_id_01", "user_name_01", "user_display_name_01", 0, 0)
+        )
+        res = client.update(val, "location_02")
+
+        val = entity.OperationInfo(
+            lambda_function.entity.ActionType.REMOVE,
+            None,
+            entity.FriendInfo("user_id_01", "user_name_01", "user_display_name_01", 0, 0)
+        )
+        res = client.update(val, "location_03")
+        client._inline_delete(res["id"])
+
+    def test_inline_method(self):
+        from src import lambda_function
+        from src.common import entity
+
+        NOTION_AUTH_TOKEN = os.getenv("NOTION_AUTH_TOKEN")
+        NOTION_PUT_DATABASEID_FRIENDLIST = os.getenv("NOTION_PUT_DATABASEID_FRIENDLIST")
+
+        val = entity.FriendInfo("user_id_01", "user_name_01", "user_display_name_01", 0, 0)
+        client = lambda_function.NotionResource(NOTION_PUT_DATABASEID_FRIENDLIST, NOTION_AUTH_TOKEN)
+        res1 = client._inline_append(val, True, "location_01")
+        assert res1 is not None
+        res2 = client._inline_append(val, False, "location_01")
+        assert res2 is not None
+
+        client = lambda_function.NotionResource(NOTION_PUT_DATABASEID_FRIENDLIST, NOTION_AUTH_TOKEN)
+        res = client._inline_find("user_id_01")
+        assert res is not None
+
+        now_time = int(datetime.now().timestamp())
+        val = entity.FriendInfo("user_id_02", "user_name_02", "user_display_name_02", now_time, now_time)
+        res1 = client._inline_update(val, True, res1)
+        assert res1 is not None
+        res2 = client._inline_update(val, False, res2)
+        assert res2 is not None
+
+        res1 = client._inline_delete(res1["id"])
+        assert res1 is not None
+        res2 = client._inline_delete(res2["id"])
+        assert res2 is not None
+
+
+def test_handler_name_1():
+    from src import lambda_function
+
+    ACCOUNT_ID = os.getenv("ACCOUNT_ID")
+    events = {
+        "Records": [
+            {
+                "body": json.dumps({
+                    "account_id": ACCOUNT_ID,
+                    "event": {
+                        "_type": "OperationInfo",
+                        "action": "ADD",
+                        "info_new": {
+                            "_type": "FriendInfo",
+                            "user_id": "usr_xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+                            "user_name": "ユーザ名",
+                            "user_display_name": "ユーザ表示名",
+                            "regist_date": 1665694831,
+                            "update_date": 1665694831
+                        },
+                        "info_old": None
+                    }
+                })
+            }
+        ]
+    }
+    lambda_function.lambda_handler(events, None)
+
+
+def test_handler_name_2():
+    from src import lambda_function
+
+    ACCOUNT_ID = os.getenv("ACCOUNT_ID")
+    events = {
+        "Records": [
+            {
+                "body": json.dumps({
+                    "account_id": ACCOUNT_ID,
+                    "event":
+                    {
+                        "_type": "OperationInfo",
+                        "action": "UPDATE",
+                        "info_new":
+                        {
+                            "_type": "FriendInfo",
+                            "user_id": "usr_xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+                            "user_name": "ユーザ名 (更新後)",
+                            "user_display_name": "ユーザ表示名 (更新後)",
+                            "regist_date": 1661942977,
+                            "update_date": 1667120431,
+                        },
+                        "info_old":
+                        {
+                            "_type": "FriendInfo",
+                            "user_id": "usr_xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+                            "user_name": "ユーザ名",
+                            "user_display_name": "ユーザ表示名",
+                            "regist_date": 1661942977,
+                            "update_date": 1661942977,
+                        },
+                    }
+                })
+            }
+        ]
+    }
+    lambda_function.lambda_handler(events, None)
+
+
+def test_handler_name_3():
+    from src import lambda_function
+
+    ACCOUNT_ID = os.getenv("ACCOUNT_ID")
+    events = {
+        "Records": [
+            {
+                "body": json.dumps({
+                    "account_id": ACCOUNT_ID,
+                    "event":
+                    {
+                        "_type": "OperationInfo",
+                        "action": "REMOVE",
+                        "info_new": None,
+                        "info_old":
+                        {
+                            "_type": "FriendInfo",
+                            "user_id": "usr_xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+                            "user_name": "ユーザ名 (更新後)",
+                            "user_display_name": "ユーザ表示名 (更新後)",
+                            "regist_date": 1661942977,
+                            "update_date": 1667120431,
+                        },
+                    }
+                })
+            }
+        ]
+    }
+    lambda_function.lambda_handler(events, None)
