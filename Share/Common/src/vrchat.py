5051aecf68393e00c77b0e7391775ffac43866a8
namoshika <namoshika@users.noreply.github.com>
2022-12-04 14:03:47 +0900

    vrchat.Service.get_friends で稀に例外が生じるバグを修正
    _internal_get_friends を通して取得するフレンド情報が get_current_user に含まれるフレンドIDリストを全て含まない事がある。含まれない場合に例外が生じるため、生じないよう含まれない分を別個に get_user で補完するよう修正。

--- Share/Common/src/vrchat.py
+++ Share/Common/src/vrchat.py
@@ -9,6 +9,7 @@ from .entity import *
 class Service:
     ENDPOINT_URL = "https://api.vrchat.cloud/api/1"
     APIKEY = "JlE5Jldo5Jibnk5O5hTx6XVqsJu4WJ26"
+    GET_USERINFO_LIMIT = 5
 
     def __init__(self, vrc_username: str, vrc_passwd: str, cookie_jar: CookieJar) -> None:
         self._vrc_username = vrc_username
@@ -33,13 +34,23 @@ class Service:
                 else:
                     raise
 
+    def get_user(self, user_id: str, update_time: datetime) -> UserInfo:
+        update_time = int(update_time.timestamp())
+        target_url = f"{self.ENDPOINT_URL}/users/{user_id}"
+        req = request.Request(target_url, method="GET")
+        res = self._opener.open(req)
+
+        dat = json.load(res)
+        inf = UserInfo(dat["id"], dat["username"], dat["displayName"], None, update_time)
+        return inf
+
     def get_friends(self, current_user_info, update_time: datetime) -> list[UserInfo]:
         # 認証情報とフレンドID一覧を取得
         friend_ids: list[str] = current_user_info["friends"]
         friend_ids.sort()
 
         # フレンドの表示名を取得
-        update_time = int(update_time.timestamp())
+        update_unixtime = int(update_time.timestamp())
         friends_map = dict()
         friends_max_length = 100
         friendsOffset = 0
@@ -47,11 +58,10 @@ class Service:
         # 取得 (オンライン)
         friendsOffset = 0
         while True:
-            apiRes = self._internal_get_friends(
-                friendsOffset, friends_max_length, False)
+            apiRes = self._internal_get_friends(friendsOffset, friends_max_length, False)
             for item in apiRes:
                 friends_map[item["id"]] = UserInfo(
-                    item["id"], item["username"], item["displayName"], None, update_time)
+                    item["id"], item["username"], item["displayName"], None, update_unixtime)
             friendsOffset += len(apiRes)
 
             if len(apiRes) != friends_max_length:
@@ -64,15 +74,21 @@ class Service:
                 friendsOffset, friends_max_length, True)
             for item in apiRes:
                 friends_map[item["id"]] = UserInfo(
-                    item["id"], item["username"], item["displayName"], None, update_time)
+                    item["id"], item["username"], item["displayName"], None, update_unixtime)
             friendsOffset += len(apiRes)
 
             if len(apiRes) != friends_max_length:
                 break
 
+        # タイミングによってはユーザ情報の取得から洩れる者が居る.
+        # 一定数以下ならば個別に補完するが、超えている場合にはエラー扱いにする.
+        miss_friend_ids = [user_id for user_id in friend_ids if user_id not in friends_map]
+        if len(miss_friend_ids) > self.GET_USERINFO_LIMIT:
+            raise Exception(f"フレンドのユーザ情報の取得に失敗. 補完最大数を超過しました ({self.GET_USERINFO_LIMIT}件以上).")
+
         # フレンド一覧 (表示名付き) を作成
         friend_infos = [
-            friends_map[user_id] or UserInfo(user_id, None, None, None, None)
+            friends_map.get(user_id) or self.get_user(user_id, update_time)
             for user_id in friend_ids
         ]
         return friend_infos
--- Share/Common/tests/test_vrchat.py
+++ Share/Common/tests/test_vrchat.py
@@ -11,8 +11,13 @@ class TestService:
         vrc_username = os.getenv("VRCHAT_USERNAME")
         vrc_password = os.getenv("VRCHAT_PASSWORD")
         vrchat = src.vrchat.Service(vrc_username, vrc_password, None)
-        res = vrchat.get_current_user()
-        assert res is not None
 
-        res = vrchat.get_friends(res, datetime.datetime.now())
-        assert len(res) > 0
+        res01 = vrchat.get_current_user()
+        assert res01 is not None
+
+        vrc_userid_sample = os.getenv("VRCHAT_USERID_SAMPLE")
+        res02 = vrchat.get_user(vrc_userid_sample, datetime.datetime.now())
+        assert res02 is not None
+        
+        res03 = vrchat.get_friends(res01, datetime.datetime.now())
+        assert len(res03) > 0
